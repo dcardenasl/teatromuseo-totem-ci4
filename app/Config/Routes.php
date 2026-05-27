495cbcf9f3c53504db4955da3f2705f1d24af757
David Cardenas <david@example.com>
2026-05-27 03:55:12 -0400

    feat(routes): configure totem routing

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,4 +5,16 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+$routes->get('/', 'TotemController::index');
+$routes->get('language', 'TotemController::language');
+$routes->get('menu', 'TotemController::mainMenu');
+$routes->get('museo', 'TotemController::museum');
+$routes->get('museo/coleccion', 'TotemController::museumCollection');
+$routes->get('museo/historia-comica', 'TotemController::museumComicHistory');
+$routes->get('museo/el-museo', 'TotemController::museumInfo');
+$routes->get('historia', 'TotemController::history');
+$routes->get('teatro-escuela', 'TotemController::theaterSchool');
+$routes->get('cartelera', 'TotemController::billboard');
+$routes->get('cartelera/detalle', 'TotemController::billboardDetail');
+$routes->get('visitas-guiadas', 'TotemController::guidedVisits');
+$routes->get('amigos-de-teatromuseo', 'TotemController::friends');
