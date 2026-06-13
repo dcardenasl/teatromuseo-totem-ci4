033611066e0956e1fab70805119710f0ed283ee3
David Cardenas <david@example.com>
2026-06-12 20:34:19 -0400

    fix(config): correct env vars, timezone and remove dead BaseController client

--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -3,8 +3,6 @@
 namespace App\Controllers;
 
 use CodeIgniter\Controller;
-use CodeIgniter\HTTP\CLIRequest;
-use CodeIgniter\HTTP\IncomingRequest;
 use CodeIgniter\HTTP\RequestInterface;
 use CodeIgniter\HTTP\ResponseInterface;
 use Psr\Log\LoggerInterface;
@@ -13,15 +11,9 @@ abstract class BaseController extends Controller
 {
     protected $request;
     protected $helpers = ['html', 'url'];
-    protected $apiClient;
 
     public function initController(RequestInterface $request, ResponseInterface $response, LoggerInterface $logger)
     {
         parent::initController($request, $response, $logger);
-
-        $this->apiClient = \Config\Services::curlrequest([
-            'base_URI' => env('API_BASE_URL'),
-            'timeout'  => 3,
-        ]);
     }
 }
